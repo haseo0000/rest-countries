1e6b92d33706be3fc1e4aa7e8723aa76a4053599
Nantapob Vankrua <nantapob007@hotmail.com>
2024-02-06 22:37:23 +0700

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["upload.wikimedia.org", "flagcdn.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+      },
+      {
+        protocol: "https",
+        hostname: "flagcdn.com",
+      },
+    ],
   },
 };
 
